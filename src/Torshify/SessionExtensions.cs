57f6e845b8279b7e50f98eb6fc12b7f737ca5946
Torstein Auensen <torshy@gmail.com>
2011-11-13 15:16:41 +0100

    Added TPL extension methods

--- src/Torshify/SessionExtensions.cs
+++ src/Torshify/SessionExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Torshify
 {
@@ -53,6 +54,125 @@ namespace Torshify
             reset.WaitOne();
             browse.Completed -= handler;
             return browse;
+        }
+
+        public static Task<ISearch> SearchAsync(
+            this ISession session,
+            string query,
+            int trackOffset,
+            int trackCount,
+            int albumOffset,
+            int albumCount,
+            int artistOffset,
+            int artistCount,
+            object userData = null)
+        {
+            var tcs = new TaskCompletionSource<ISearch>(userData);
+
+            var search = session.Search(
+                query,
+                trackOffset,
+                trackCount,
+                albumOffset,
+                albumCount,
+                artistOffset,
+                artistOffset,
+                userData);
+            search.Completed += (sender, args) => tcs.SetResult(search);
+            return tcs.Task;
+        }
+
+        public static Task<ISearch> SearchAsync(
+            this ISession session, 
+            int fromYear,
+            int toYear,
+            RadioGenre genre,
+            object userData = null)
+        {
+            var tcs = new TaskCompletionSource<ISearch>();
+
+            var search = session.Search(
+                fromYear,
+                toYear,
+                genre,
+                userData);
+            search.Completed += (sender, args) => tcs.SetResult(search);
+            return tcs.Task;
+        }
+
+        public static Task<IArtistBrowse> BrowseAsync(
+            this ISession session, 
+            IArtist artist, 
+            ArtistBrowseType type,
+            object userState = null)
+        {
+            var tcs = new TaskCompletionSource<IArtistBrowse>();
+
+            var browse = session.Browse(artist, type, userState);
+            browse.Completed += (sender, args) => tcs.SetResult(browse);
+            return tcs.Task;
+        }
+
+        public static Task<IAlbumBrowse> BrowseAsync(
+            this ISession session, 
+            IAlbum album,
+            object userState = null)
+        {
+            var tcs = new TaskCompletionSource<IAlbumBrowse>();
+
+            var browse = session.Browse(album, userState);
+            browse.Completed += (sender, args) => tcs.SetResult(browse);
+            return tcs.Task;
+        }
+
+        public static Task<IToplistBrowse> BrowseAsync(
+            this ISession session,
+            ToplistType toplist,
+            object state = null)
+        {
+            var tcs = new TaskCompletionSource<IToplistBrowse>();
+
+            var browse = session.Browse(toplist, state);
+            browse.Completed += (sender, args) => tcs.SetResult(browse);
+            return tcs.Task;
+        }
+
+        public static Task<IToplistBrowse> BrowseAsync(
+            this ISession session, 
+            ToplistType type,
+            int encodedCountryCode, 
+            object userData = null)
+        {
+            var tcs = new TaskCompletionSource<IToplistBrowse>();
+
+            var browse = session.Browse(type, encodedCountryCode, userData);
+            browse.Completed += (sender, args) => tcs.SetResult(browse);
+            return tcs.Task;
+        }
+
+        public static Task<IToplistBrowse> BrowseAsync(
+            this ISession session, 
+            ToplistType type, 
+            string userName, 
+            object userData = null)
+        {
+            var tcs = new TaskCompletionSource<IToplistBrowse>();
+
+            var browse = session.Browse(type, userName, userData);
+            browse.Completed += (sender, args) => tcs.SetResult(browse);
+            return tcs.Task;
+        }
+
+        public static Task<IToplistBrowse> BrowseCurrentUserAsync(
+            this ISession session, 
+            ToplistType type, 
+            object userData = null)
+        {
+            var tcs = new TaskCompletionSource<IToplistBrowse>();
+
+            var browse = session.BrowseCurrentUser(type, userData);
+            browse.Completed += (sender, args) => tcs.SetResult(browse);
+            return tcs.Task;
         }
     }
 }
